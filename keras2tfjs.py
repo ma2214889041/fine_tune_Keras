a4d461d4b85ddf80de6a536e8a5f0edbb33cb151
Ma Qiang <ma2214889041@gmail.com>
2022-09-28 01:46:11 +0200

    Delete keras2tfjs.py

--- keras2tfjs.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-from tensorflow import keras
-import tensorflowjs as tfjs
-from tensorflow.keras.models import load_model
-
-mobile_num = load_model('models/mobile_num.h5')
-
-tfjs.converters.save_keras_model(mobile_num ,'models/tfjs_models')
